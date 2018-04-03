2add55db74c404d377b40d4f71f490a6ea7c1594
Faip Mardoni <faipmardoni@gmail.com>
2018-04-03 15:35:30 +0700

    Binary Search

--- binary.js
+++ binary.js
@@ -5,26 +5,61 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  // console.log(arr)
   return arr
 }
 
-function binarySearch (search, array) {
+function binarySearch(search, array) {
   // Your searching code
-  return 0;
+  let mid = Math.floor((array.length-1)/2);
+  if (array[mid] === search) {
+    return mid
+  }else if(search > array[mid]) {
+    let awal = mid;
+    mid = Math.floor(((array.length-1)-(mid))/2)
+    mid = mid+awal;
+    if (array[mid] === search) {
+      return mid
+    }else if(search > array[mid]) {
+      let awal = mid;
+      mid = Math.floor(((array.length-1)-(mid))/2)
+      mid = mid+awal;
+      if (array[mid] === search) {
+        return mid
+      }else {
+        return -1
+      }
+    }
+  }else if(search < array[mid]) {
+    // console.log(a)
+    let akhir = mid;
+    mid = Math.floor(((array.length-1)-(mid))/2);
+  }
 }
-
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
+var arrayGenapSorted = ownSort(testArrayGenap);
+var arrayGanjilSorted = ownSort(testArrayGanjil);
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(3, [1,2,3,4,5]) === 2) // true
+console.log(binarySearch(35, [13,19,24,29,32,37,43]) === -1) //true
+console.log(binarySearch(135, [100,120,130,135,150,170]) === 3) //true
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+// console.log(binarySearch(8, arrayGenapSorted))
+// console.log(binarySearch(10, arrayGenapSorted))
+// console.log(binarySearch(33, arrayGenapSorted))
+// console.log(binarySearch(53, arrayGanjilSorted))
+// console.log(binarySearch(3, arrayGanjilSorted))
+// console.log(binarySearch(2, arrayGanjilSorted))
 
 module.exports = {
-  binary_search
+  binarySearch
 }
