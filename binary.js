79bd9f0ee4be3b99f76db6c77fc168c49718897a
asepridwan1989 <aseprmail@gmail.com>
2018-04-03 12:47:43 +0700

    upload done

--- binary.js
+++ binary.js
@@ -5,18 +5,44 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  var count1 = 0
+  while(count1 < arr.length){
+    var value = arr[count1]
+    var count2 = count1 -1
+    while(count2 >=0 && arr[count2] > value){
+      arr[count2+1] = arr[count2]
+      count2 --
+    }
+    arr[count2+1] = value
+    count1++
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var min = 0
+  var max = array.length-1
+  while (min <= max) {
+    var middle = Math.floor((min + max)/2)
+    if (array[middle] === search) {
+      return middle
+    }
+    if (array[middle] < search) {
+      min = middle + 1
+    } else {
+      max = middle - 1
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
+// console.log(arrayGenapSorted)
+// console.log(arrayGanjilSorted)
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
