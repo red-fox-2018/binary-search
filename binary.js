6651c41974618b0cb3e3d2168fb7fe16e9c17f79
Iswanul Umam <umam@sepulsa.com>
2018-04-03 16:09:41 +0700

    release 1

--- binary.js
+++ binary.js
@@ -55,6 +55,27 @@ console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
 
-// module.exports = {
-//   binary_search
-// }
+console.log(binary_search(3, [1, 2, 3, 4, 5]) == 2); // true
+console.log(binary_search(35, [13, 19, 24, 29, 32, 37, 43]) == -1); // false
+console.log(binary_search(135, [100, 120, 130, 135, 150, 170]) == 3) // true
+
+/*
+
+RELEASE 1: 
+
+INIT data = [13, 19, 24, 29, 32, 37, 43]
+INIT value = 32
+
+INIT left = 0
+INIT right = data.length - 1
+INIT mid = left + (right - left) / 2   --> [mid = 3]
+
+step 1: check data[] index mid, nilai yang didapat adalah 29, data belum ditemukan jadi pencarian dilanjutkan
+step 2: check data[] index mid, kondisi yang didapat true, sehingga nilai left = mid + 1
+step 3: assign nilai mid = left + (right - left) / 2 (mid -> 3) --> [mid = 5]
+step 4: check data[] index mid, nilai yang didapat adalah 73, data belum ditemukan jadi pencarian dilanjutkan
+step 5: check data[] index mid < search, kondisi yang didapat false, sehingga nilai right = mid - 1
+step 6: assign nilai mid = left + (right - left) / 2 (mid -> 3) --> [mid = 4]
+step 7: check data[] index mid, nilai yang didapat adalah 32, data ditemukan dan pencarian berhenti
+
+*/
